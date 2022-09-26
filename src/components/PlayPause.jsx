0ce8cc1fdd41411f144375dc9a8bfa503d680865
denis <denis@kaizen-design.ru>
2022-09-27 01:21:40 +0300

    Discover page bootstraping

--- src/components/PlayPause.jsx
+++ src/components/PlayPause.jsx
@@ -1,5 +1,19 @@
-const PlayPause = () => (
-  <div>Loader</div>
+import { FaPauseCircle, FaPlayCircle } from 'react-icons/fa';
+
+const PlayPause = ({ song, handlePause, handlePlay, activeSong, isPlaying }) => (
+  isPlaying && activeSong?.title === song.title ? (
+    <FaPauseCircle
+      size={35}
+      className="text-gray-300"
+      onClick={handlePlay}
+    />
+  ) : (
+    <FaPlayCircle
+      size={35}
+      className="text-gray-300"
+      onClick={handlePause}
+    />
+  )
 );
 
 export default PlayPause;
---
+++ src/redux/services/shazamCore.js
@@ -0,0 +1,19 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+export const shazamCoreApi = createApi({
+  reducerPath: 'shazamCoreApi',
+  baseQuery: fetchBaseQuery({
+    baseUrl: 'https://shazam-core.p.rapidapi.com/v1',
+    prepareHeaders: (headers) => {
+      headers.set('X-RapidAPI-Key', 'ef00a6bbe3mshe46278e331c67d7p1ecdb0jsnb760f22e05ca');
+      return headers;
+    },
+  }),
+  endpoints: (builder) => ({
+    getTopCharts: builder.query({ query: () => '/charts/world' }),
+  }),
+});
+
+export const {
+  useGetTopChartsQuery,
+} = shazamCoreApi;
